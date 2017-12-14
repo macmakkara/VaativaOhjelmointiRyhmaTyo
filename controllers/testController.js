da15dfdd4b6224aad8ad691a67b1f7121eae394a
janibala <jani_bala@hotmail.com>
2017-12-15 00:54:55 +0200

    Lisätty debug palikoita ja jemmattu console.logeja

--- controllers/testController.js
+++ controllers/testController.js
@@ -5,7 +5,7 @@ module.exports = {
 
     testconnection: function (req, res) {
         //Tätä voi käyttää clientin puolella testaamaan, onko palvelin käynnissä. Lähinnä sanitycheck tarkoituksissa
-        console.log("yhteyttä testataan osoitteesta:" + req.hostname);
+        //console.log("yhteyttä testataan osoitteesta:" + req.hostname);
         res.status(200).json("Palvelin on toiminnassa");
     },
 
@@ -18,9 +18,38 @@ module.exports = {
             console.log(vastaus);
 
             if (virhe) {
-                res.status(500).json("Tapahtui virhe tyhjentäessä collectionia: " + virhe);
+                res.status(500).json("Tapahtui virhe tyhjentäessä pistedataa: " + virhe);
             } else {
                 res.status(200).json("Collection tyhjennetty onnistuneesti");
+            }
+        })
+    },
+
+    deleteAllGames: function (req, res) {
+        console.log("deleteAllGames");
+
+        db_controller.deleteAllGames((virhe, vastaus) => {
+            console.log(virhe);
+            console.log(vastaus);
+
+            if (virhe) {
+                res.status(500).json("Tapahtui virhe tyhjentäessä pelidataa: " + virhe);
+            } else {
+                res.status(200).json("Collection tyhjennetty onnistuneesti");
+            }
+        })
+    },
+
+    emptyDatabase: function (req, res) {
+        console.log("emptyDatabase");
+
+        db_controller.emptyDatabase((virhe, vastaus) => {
+
+
+            if (virhe) {
+                res.status(500).json("Tapahtui virhe tyhjentäessä kantaa: " + virhe);
+            } else {
+                res.status(200).json("Kanta tyhjennetty onnistuneesti");
             }
         })
     },
