5f3d5bd67a0c743ba6412f46ae6726af93954ad4
janibala <jani_bala@hotmail.com>
2017-12-15 00:53:28 +0200

    Lisätty post parametrien validointia

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -6,42 +6,47 @@ module.exports = {
 
     //lähettää uuden pelin kantaan, palauttaa kyseisen pelin id:n (ns. gametoken jota pelintekijä voi hyödyntää)
     addGame: function (req, res) {
-        console.log("addGame");
-        console.log(req.body);
+        //console.log("addGame:");
+        //console.log(req.body);
 
+        if (!req.body.peli_nimi) {
+            res.status(400).json("Pelin nimi on pakollinen tieto!");
 
-        //Tarkistetaan onko peli jo kannassa
-        db_controller.findGameByName(req.body.peli_nimi, (virhe, vastaus) => {
+         } else {
 
-            if (virhe) {
-                res.status(500).json("Tapahtui virhe tarkistaessa pelin nimen olemassaoloa:" + virhe);
-            } else {
+            //Tarkistetaan onko peli jo kannassa
+            db_controller.findGameByName(req.body.peli_nimi, (virhe, vastaus) => {
 
-                if (vastaus.length > 0) {
-                    console.log(vastaus);
-                    res.status(500).json("Samanniminen peli on jo olemassa! Kokeile toista nimeä");
+                if (virhe) {
+                    res.status(500).json("Tapahtui virhe tarkistaessa pelin nimen olemassaoloa:" + virhe);
                 } else {
 
-                    let uusipeli = {
-                        "peli_nimi": req.body.peli_nimi,
-                        "timestamp": moment().format("x"),
-                        "gametoken": String(Math.random().toString(36).substr(2) + moment().format("x"))   //Tähän kunnollinen logiikka tilalle, nyt lähinnä "proof of concept" token;
-                    };
+                    if (vastaus.length > 0) {
+                        //console.log(vastaus);
+                        res.status(400).json("Samanniminen peli on jo olemassa! Kokeile toista nimeä");
+                    } else {
 
-                    db_controller.addGame(uusipeli, (virhe, vastaus) => {
+                        let uusipeli = {
+                            "peli_nimi": req.body.peli_nimi,
+                            "timestamp": moment().format("x"),
+                            "gametoken": String(Math.random().toString(36).substr(2) + moment().format("x"))   //Tähän kunnollinen logiikka tilalle, nyt lähinnä "proof of concept" token;
+                        };
 
-                        if (virhe) {
-                            res.status(500).json("Tapahtui virhe lisättäessä peliä. Virhe: " + virhe);
-                        } else {
-                            //palauttaa tuon aiemmin tehdyn tokenin
-                            res.status(200).json(uusipeli.gametoken);
-                        }
+                        db_controller.addGame(uusipeli, (virhe, vastaus) => {
 
-                    })
+                            if (virhe) {
+                                res.status(500).json("Tapahtui virhe lisättäessä peliä. Virhe: " + virhe);
+                            } else {
+                                //palauttaa tuon aiemmin tehdyn tokenin
+                                res.status(200).json(uusipeli.gametoken);
+                            }
+
+                        })
+                    }
                 }
-            }
 
-        })
+            })
+        }
     },
 
 
