dd217e9e106f91b0565f9f35e660afeb1a697c48
janibala <jani_bala@hotmail.com>
2017-12-18 22:24:29 +0200

    Muotoilua ja refaktorointia

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -4,28 +4,31 @@ const mongodb = require("mongodb");
 const ObjectId = require('mongodb').ObjectID;
 
 module.exports = {
-    //TODO: Callback helvetin välttämiseksi pitäisi varmaan siirtyä promisen käyttöön.
 
-    //lähettää uuden pelin kantaan, palauttaa lisätyn pelin tiedot, jolla lisätä pisteitä.
+    //lähettää uuden pelin kantaan, palauttaa lisätyn ns. autentikointitiedot
     addGame: function (req, res) {
-        //console.log("addGame:");
-        //console.log(req.body);
 
         if (!req.body.peli_nimi) {
+
             res.status(400).json("Pelin nimi on pakollinen tieto!");
 
         } else {
 
             //Tarkistetaan onko samanniminen peli jo kannassa
-            db_controller.findGameByName(req.body.peli_nimi, (virhe, vastaus) => {
+            db_controller.findGameByName(req.body.peli_nimi, (virhe_findGameByName, vastaus_findGameByName) => {
 
-                if (virhe) {
-                    res.status(500).json("Tapahtui virhe tarkistaessa pelin nimen olemassaoloa:" + virhe);
+                if (virhe_findGameByName) throw virhe_findGameByName;
+
+                if (virhe_findGameByName) {
+
+                    res.status(500).json("Tapahtui virhe tarkistaessa pelin nimen olemassaoloa:" + virhe_findGameByName);
+
                 } else {
 
-                    if (vastaus.length > 0) {
-                        //console.log(vastaus);
+                    if (vastaus_findGameByName.length > 0) {
+
                         res.status(400).json("Samanniminen peli on jo olemassa! Kokeile toista nimeä");
+
                     } else {
 
                         let uusipeli = {
@@ -34,36 +37,49 @@ module.exports = {
                             "gametoken": String(Math.random().toString(36).substr(2) + moment().format("x"))   //Tähän kunnollinen logiikka tilalle, nyt lähinnä "proof of concept" token;
                         };
 
-                        db_controller.addGame(uusipeli, (virhe, vastaus) => {
+                        db_controller.addGame(uusipeli, (virhe_addGame, vastaus_addGame) => {
 
-                            if (virhe) {
-                                res.status(500).json("Tapahtui virhe lisättäessä peliä. Virhe: " + virhe);
+                            if (virhe_addGame) throw virhe_addGame;
+
+                            if (virhe_addGame) {
+
+                                res.status(500).json("Tapahtui virhe lisättäessä peliä. Virhe: " + virhe_addGame);
+
                             } else {
 
-
                                 let authData = {
-                                    "game_id": vastaus.ops[0]._id,
-                                    "gametoken": vastaus.ops[0].gametoken
+                                    "game_id": vastaus_addGame.ops[0]._id,
+                                    "gametoken": vastaus_addGame.ops[0].gametoken
                                 };
 
                                 res.status(200).json(authData);
                             }
 
-                        })
+                        });
+
                     }
                 }
 
-            })
+            });
+
         }
+
     },
 
     getGameList: function (req, res) {
 
-        db_controller.getGameList((virhe, vastaus) => {
-            if (virhe) {
-                res.status(500).json(virhe);
+        db_controller.getGameList((virhe_getGameList, vastaus_getGameList) => {
+
+            if (virhe_getGameList) throw virhe_getGameList;
+
+            if (virhe_getGameList) {
+
+                res.status(500).json("Tapahtui virhe hakiessa pelien listaa: " + virhe_getGameList);
+
             } else {
-                res.status(200).json(vastaus);
+
+                res.status(200).json(vastaus_getGameList);
+
             }
         });
     },
@@ -78,22 +94,30 @@ module.exports = {
 
         } else {
 
-            db_controller.findGameById(req.params.game_id, (virhe, vastaus) => {
+            db_controller.findGameById(req.params.game_id, (virhe_findGameById, vastaus_findGameById) => {
 
-                if (virhe) {
-                    res.status(500).json("Tapahtui virhe haettaessa peliä. Virhe: " + virhe);
+                if (virhe_findGameById) {
+
+                    res.status(500).json("Tapahtui virhe haettaessa peliä. Virhe: " + virhe_findGameById);
+
                 } else {
 
-                    if (vastaus.length == 0) {
+                    if (vastaus_findGameById.length == 0) {
+
                         res.status(404).json("Peliä ei ole olemassa!")
+
                     } else {
-                        res.status(200).json(vastaus);
+
+                        res.status(200).json(vastaus_findGameById);
+
                     }
 
                 }
-            })
-        }
-    },
 
+            });
+
+        }
+
+    }
 
 };
