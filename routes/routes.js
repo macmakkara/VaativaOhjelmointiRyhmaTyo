40f30ced8a4f3ebea1bca08516bccc6feb47ac17
janibala <jani_bala@hotmail.com>
2017-12-18 22:26:03 +0200

    Kommenttien päivittelyä ja järjestyksien hifistelyä

--- routes/routes.js
+++ routes/routes.js
@@ -1,16 +1,14 @@
 const express = require('express');
+const router = express.Router();
 const testController = require("../controllers/testController");
 const scoreController = require("../controllers/scoreController");
 const gameController = require("../controllers/gameController");
-const router = express.Router();
-
 
 /* Testauspalikat yms. satunnainen debug sälä */
 router.route('/testconnection').get(testController.testconnection);
 router.route('/deleteAllScores').get(testController.deleteAllScores);
 router.route('/deleteAllGames').get(testController.deleteAllGames);
 router.route('/emptyDatabase').get(testController.emptyDatabase);
-
 
 
 /* Public puoli */
@@ -20,7 +18,7 @@ router.route('/getplayerscore/:playername/:game_id?').get(scoreController.getPla
 router.route('/getGameList').get(gameController.getGameList);
 router.route('/getGameById/:game_id').get(gameController.findGameById);
 
-/*Admin puoli. Pelien lisääminen jne. */
+/* ns. Admin puoli. Pelien ja pisteiden lisääminen */
 router.route('/addgame').post(gameController.addGame);
 router.route('/addplayerscore').post(scoreController.addPlayerScore);
 
