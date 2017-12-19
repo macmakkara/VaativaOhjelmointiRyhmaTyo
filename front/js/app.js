535dc106c4a612f70d96eec4bae2630c601a484a
Salonen Joni <ojosa002@edu.xamk.fi>
2017-12-19 12:07:50 +0200

    palautettu api pathi localhostiksi

--- front/js/app.js
+++ front/js/app.js
@@ -1,5 +1,5 @@
 var main = angular.module("main", ['angular-growl', 'ngAnimate']);
-var apiUrl = "http://seiz.in:8080/api/";
+var apiUrl = "http://localhost:3333/api/";
 
 
 main.config(['growlProvider', function (growlProvider) {
